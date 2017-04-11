e0c9f1ce242f7ac216ebb908378152c6d81d2668
Guilherme Eduardo Rocha <e.guilherme@pucpr.edu.br>
2017-04-11 20:42:42 -0300

    Adicionei função soma

--- Teste GitHub/Teste GitHub/main.cpp
+++ Teste GitHub/Teste GitHub/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
-int main() {
+int calculaSoma(int a, int b) {
+	return a + b;
+}
 
+int main() {
+	int resultado;
 	std::cout << "teste gitHub;" << std::endl;
+
+	resultado = calculaSoma(5, 8);
+
+	std::cout << "resultado: " << resultado << std::endl;
 
 	return 0;
 }
