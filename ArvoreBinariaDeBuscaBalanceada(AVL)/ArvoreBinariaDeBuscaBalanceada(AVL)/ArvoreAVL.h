5cd6d5ee8914ae1d5dd2d5d8ca831eb4332fa90c
Guilherme Eduardo Rocha <e.guilherme@pucpr.edu.br>
2017-04-11 21:01:47 -0300

    Correção de erros

--- ArvoreBinariaDeBuscaBalanceada(AVL)/ArvoreBinariaDeBuscaBalanceada(AVL)/ArvoreAVL.h
+++ ArvoreBinariaDeBuscaBalanceada(AVL)/ArvoreBinariaDeBuscaBalanceada(AVL)/ArvoreAVL.h
@@ -10,17 +10,17 @@ public:
 
 
 	//Metodos a serem implementados:
-	void insere_elemento(ArvoreAVL, Elemento);
+	void insere_elemento(ArvoreAVL, int Elemento);
 
-	int remove_elemento(ArvoreAVL, Elemento);
+	int remove_elemento(ArvoreAVL, int Elemento);
 
-	bool existe_elemento(No AVL, Elemento);  // verifica se um elemento existe na árvore
+	bool existe_elemento(No AVL, int Elemento);  // verifica se um elemento existe na árvore
 
 	std::string imprime_preordem(No AVL); // imprime os elementos da árvore em preordem
 
-	std::string(No AVL); // imprime os elementos da árvore em inordem
+	std::string  imprime_inordem(No AVL); // imprime os elementos da árvore em inordem
 
-	std::string(No AVL); // imprime os elementos da árvore em posordem
+	std::string imprime_posordem(No AVL); // imprime os elementos da árvore em posordem
 
 	int altura(No AVL); // retorna a altura da árvore.
 };
